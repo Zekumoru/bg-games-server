48b2a8d8ba8f90b9ed77d50a85acf7d573d3c266
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-23 13:22:58 +0200

    Implement card update route

--- routes/shuffler/card.ts
+++ routes/shuffler/card.ts
@@ -1,9 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 import ShufflerCard, { IShufflerCard } from '../../models/ShufflerCard';
+import { Document, FilterQuery, Model } from 'mongoose';
 
 const cardRouter = express.Router();
+
+const handleValidationError = (errMessage: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
+        status: 422,
+        message: errMessage,
+        errors: errors.mapped(),
+      });
+      return;
+    }
+
+    next();
+  };
+};
 
 const extractCard = ({ name, createdAt }: IShufflerCard) => ({
   name,
@@ -17,27 +34,16 @@ const nameValidations = body('name')
   .withMessage(`name is required`)
   .isLength({ max: 3000 })
   .withMessage(`name cannot have more than 3000 characters`)
-  .escape()
-  .custom(async (name) => {
-    const card = await ShufflerCard.findOne({ name });
-    if (card) throw new Error(`Card '${name}' already exists`);
-  });
+  .escape();
 
 cardRouter.post(
   '/create',
-  nameValidations,
+  nameValidations.custom(async (name) => {
+    const card = await ShufflerCard.findOne({ name });
+    if (card) throw new Error(`Card '${name}' already exists`);
+  }),
+  handleValidationError('Card fields have errors.'),
   asyncHandler(async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(422).json({
-        status: 422,
-        message: 'Card fields have errors.',
-        errors: errors.mapped(),
-      });
-      return;
-    }
-
     const card = new ShufflerCard({ name: req.body.name });
     await card.save();
 
@@ -45,6 +51,50 @@ cardRouter.post(
       status: 201,
       message: `Card '${card.name}' has been created successfully!`,
       card: extractCard(card),
+    });
+  })
+);
+
+// declare global type for express request 'card' property
+type TShufflerCardFind = Document<IShufflerCard> & FilterQuery<IShufflerCard>;
+declare global {
+  namespace Express {
+    interface Request {
+      card: TShufflerCardFind;
+    }
+  }
+}
+
+cardRouter.post(
+  '/:name/update',
+  // check if this card already exists
+  asyncHandler(async (req, res, next) => {
+    req.card = (await ShufflerCard.findOne<IShufflerCard>({
+      name: req.params.name,
+    })) as TShufflerCardFind;
+
+    if (!req.card) {
+      res.status(422).json({
+        status: 422,
+        message: `Card '${req.params.name}' does not exist`,
+      });
+      return;
+    }
+
+    next();
+  }),
+  nameValidations.custom(async (name) => {
+    const card = await ShufflerCard.findOne({ name });
+    if (card) throw new Error(`Card '${name}' already exists`);
+  }),
+  handleValidationError('Card fields have errors.'),
+  asyncHandler(async (req, res) => {
+    req.card.overwrite({ name: req.body.name });
+    await req.card.save();
+
+    res.json({
+      status: 200,
+      message: `Card '${req.params.name}' has been updated successfully to '${req.body.name}'!`,
     });
   })
 );
